2d1a29dc9b3305e95b75b096445daa192f3ab288
P3ronch00 <112597374+P3ronch00@users.noreply.github.com>
2023-08-29 21:46:32 -0300

    Update menu_app_usuario.py

--- Python/Semana01/menu_app_usuario.py
+++ Python/Semana01/menu_app_usuario.py
@@ -19,5 +19,17 @@ while opcion != 5:
         usuario = Usuario(username=username_var, password=password_var)
         usuario_insertado = usuarioDAO.insertar(usuario)
         log.info(f'Usuario insertado: {usuario_insertado}')
+    elif opcion == 3:
+        id_usuario_var = int(input('Digite el id del usuario a modificar: '))
+        username_var = input('Digite el nombre del usuario a modificar: ')
+        password_var = input('Digite la contraseña del usuario a modificar: ')
+        usuario = Usuario(id_usuario=id_usuario_var, username=username_var, password=password_var)
+        usuario_actualizado = UsuarioDAO.actualizar(usuario)
+        log.info(f'Usuario actualizado: {usuario_actualizado}')
+    elif opcion == 4:
+        id_usuario_var = int(input('Digite el id del usuario a eliminar: '))
+        usuario = Usuario(id_usuario=id_usuario_var)
+        usuario_eliminado = UsuarioDAO.eliminar(usuario)
+        log.info(f'Usuario eliminado: {usuario_eliminado}')
 else:
     log.info('Salimos de la aplicacion, Hasta pronto!!!')
