dfddc0e32c4dc425e3e49e0e3b42198e0f2f1ead
P3ronch00 <112597374+P3ronch00@users.noreply.github.com>
2023-09-06 12:32:42 -0300

    Update Sistemas_numeracion.py

--- Python/Semana02/Sistemas_numeracion.py
+++ Python/Semana02/Sistemas_numeracion.py
@@ -15,3 +15,23 @@ print(f'a octal: {a}')
 # Sistema hexadecimal
 a = 0xA
 print(f'a hexadecimal: {a}')
+
+# Base decimal
+a = int('23', 10)
+print(f"a decimal: {a}")
+
+# Base binario
+a = int('10111', 2)
+print(f"a binario: {a}")
+
+# Base octal
+a = int('27', 8)
+print(f"a octal: {a}")
+
+# Base exadecimal
+a = int('17', 16)
+print(f"a hexadecimal: {a}")
+
+# Base 5 sus valores de 0 a 4
+a = int('344', 5)
+print(f"a Base 5: {a}")
