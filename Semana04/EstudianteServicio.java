5650a8d73d24553390888c9cd540fc5a5e5779e0
Nicolás Castro <113069134+nicoocastro@users.noreply.github.com>
2023-09-11 21:42:51 -0300

    añadimos la semana04

---
+++ Semana04/EstudianteServicio.java
@@ -0,0 +1,36 @@
+package utn.estudiantes.servicio;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.repositorio.EstudianteRepositorio;
+
+import java.util.List;
+
+@Service
+public class EstudianteServicio implements IEstudianteServicio{
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
+
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+        return null;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+
+    }
+}
