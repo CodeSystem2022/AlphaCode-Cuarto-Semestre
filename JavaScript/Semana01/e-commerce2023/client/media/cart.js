1f4b025eee37c845c1c5d5b3ccac241f8a0d2699
Gimenap <113073790+Gimenap@users.noreply.github.com>
2023-09-18 21:48:50 -0300

    Update cart.js

--- JavaScript/Semana01/e-commerce2023/client/media/cart.js
+++ JavaScript/Semana01/e-commerce2023/client/media/cart.js
@@ -1,8 +1,12 @@
 const modalContainer = document.getElementById("modal-container");
 const modalOverlay = document.getElementById("modal-overlay");
 
+const cartBtn = document.getElementById("cart-btn");
 
 const displayCart = () => {
+        modalContainer.innerHTML = "";
+  modalContainer.style.display = "block";
+  modalContainer.style.display = "block";
     //modal Header
     const modalHeader = document.createElement("div");
     
@@ -10,5 +14,12 @@ const displayCart = () => {
     modalClose.innerText = "❌";
     modalClose.className = "modal-close";
     modalHeader.append(modalClose);
+
+  modalClose.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+    modalOverlay.style.display = "none";
+  });
+
+  const modalTitle = document.createElement("div");
         
 };
