ed732684be4e20ebb2ce5910f13492716d116431
Carla Rosales <112598258+Carla052@users.noreply.github.com>
2023-09-25 19:39:39 -0300

    EstudianteRepositorio.java

--- Semana 03/EstudianteRepositorio.java
+++ Semana 03/EstudianteRepositorio.java
@@ -1,7 +1,6 @@
 package utn.estudiantes.repositorio;
 
 import utn.estudiantes.modelo.Estudiante;
-import org.springframework.data.jpa.repository.JpaRepository;
 import utn.estudiantes.modelo.Estudiante;
 
 public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer>{
