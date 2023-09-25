36d42c846c8ec8c940c502b01a542f911ee9cb07
P3ronch00 <112597374+P3ronch00@users.noreply.github.com>
2023-09-25 19:00:49 -0300

    Update EstudianteRepositorio.java

--- Semana 03/EstudianteRepositorio.java
+++ Semana 03/EstudianteRepositorio.java
@@ -1,6 +1,8 @@
 package utn.estudiantes.repositorio;
 
-import utn.estudiantes.modelo.Estudiante;  
+import utn.estudiantes.modelo.Estudiante;
+import org.springframework.data.jpa.repository.JpaRepository;
+import utn.estudiantes.modelo.Estudiante;
 
 public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer>{
 }
