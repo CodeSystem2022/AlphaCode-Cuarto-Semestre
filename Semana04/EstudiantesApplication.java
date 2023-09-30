5f19c63fa33e0fae5ecb8ae90c1a44519e0d631f
maturanoeric <111548587+maturanoeric@users.noreply.github.com>
2023-09-30 18:26:37 -0300

    Update EstudiantesApplication.java

--- Semana04/EstudiantesApplication.java
+++ Semana04/EstudiantesApplication.java
@@ -6,13 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  
 @SpringBootApplication
 public class EstudiantesApplication implements CommandLineRunner {
- 
+    @Autowired
+    private EstudianteServicio estudianteServicio;
+    private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(EstudiantesApplication.class, args);
     }
  
     @Override
     public void run(String... args) throws Exception {
+        logger.info(nl+"Ejecutando el método run de Spring..."+nl);
     }
 }
 
@@ -26,8 +30,6 @@ public class EstudiantesApplication implements CommandLineRunner {
 
 
  
- @Autowired
-    private EstudianteServicio estudianteServicio;
-    private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+
 
    
