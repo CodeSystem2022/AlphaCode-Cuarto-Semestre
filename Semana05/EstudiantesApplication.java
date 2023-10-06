66fe238e1da10dad51367c48d82fc7e1965f2289
Nati88 <113054513+Nati88@users.noreply.github.com>
2023-10-06 15:01:17 -0300

    Update EstudiantesApplication.java

--- Semana05/EstudiantesApplication.java
+++ Semana05/EstudiantesApplication.java
@@ -8,60 +8,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.modelo.Estudiante2022;
 import utn.estudiantes.servicio.EstudianteServicio;
+
+import java.util.list;
+import java.util.Scanner;
 
 
 @SpringBootApplication
-
 public class EstudiantesApplication implements CommandLineRunner {
 	@Autowired
 	private EstudianteServicio estudianteServicio;
-	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+	private static final Logger 
+		logger = LoggerFactory.getLogger(EstudiantesApplication.class);
 
-	String nl = System.lineSeparator();
+String nl = System.lineSeparator();
 
-	public static void main(String[] args) {
-		logger.info("Iniciando la aplicación...");
-		//Levantar la fabrica de Spring
-		SpringApplication.run(EstudiantesApplication.class, args);
-		logger.info("Aplicación finalizada!");
-	}
-	@Override
-	public void run(String... args) throws Exception {
-		logger.info(nl+"Ejecutando el método run de Spring..."+nl);
-       	        var salir = false;
-        	var consola = new Scanner(System.in);
-        	while(!salir){
-	            mostrarMenu();
-	            salir = ejecutarOpciones(consola);
-	            logger.info(nl);
-	        }//Fin ciclo while
-	}
+public static void main(String[] args) {
+	logger.info("Iniciando la aplicación...");
+	//Levantar la fabrica de Spring
+	SpringApplication.run(EstudiantesApplication.class, args);
+	logger.info("Aplicación finalizada!");
+}
+@Override
+public void run(String... args) throws Exception {
+	logger.info(nl+"Ejecutando el método run de Spring..."+nl);
+       	var salir = false;
+        var consola = new Scanner(System.in);
+        while(!salir){
+	        mostrarMenu();
+	        salir = ejecutarOpciones(consola);
+	        logger.info(nl);
+	}//Fin ciclo while
+}
 	
-	private void mostrarMenu(){
-		logger.info(nl);
-		logger.info("""
-			******* Sistema de Estudiantes *******
-			1. Listar Estudiantes
-			2. Buscar Estudiante
-			3. Agregar Estudiante
-			4. Modificar Estudiante
-			5. Eliminar Estudiante
-			6. Salir
-			Eliga una opción:""");
-	}
+private void mostrarMenu(){
+	logger.info(nl);
+	logger.info("""
+		******* Sistema de Estudiantes *******
+		1. Listar Estudiantes
+		2. Buscar Estudiante
+		3. Agregar Estudiante
+		4. Modificar Estudiante
+		5. Eliminar Estudiante
+		6. Salir
+		Eliga una opción:""");
+}
 	
-	private boolean ejecutarOpciones(Scanner consola){
-        	var opcion = Integer.parseInt(consola.nextLine());
-        	var salir = false;
-        	switch (opcion){
-            		case 1 -> {//Listar estudiantes
-                	logger.info(nl+"Listado de estudiantes: "+nl);
-                	List<Estudiantes2022> estudiantes = estudianteServicio.listarEstudiantes();
-                	estudiantes.forEach((estudiante -> logger.info(estudiante.toString()+nl)));
-            		}
-        	}//Fin switch
-        	return salir;
-    	}
+private boolean ejecutarOpciones(Scanner consola){
+	var opcion = Integer.parseInt(consola.nextLine());
+        var salir = false;
+        switch (opcion){
+            case 1 -> {//Listar estudiantes
+                logger.info(nl+"Listado de estudiantes: "+nl);
+                List<Estudiantes2022> estudiantes = estudianteServicio.listarEstudiantes();
+                estudiantes.forEach((estudiante -> logger.info(estudiante.toString()+nl)));
+            }
+        }//Fin switch
+        return salir;
+}
 
