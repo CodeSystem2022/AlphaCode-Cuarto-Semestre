450c5ea8e0f3d24363f0f60d5ff1466c3fd2a29e
P3ronch00 <112597374+P3ronch00@users.noreply.github.com>
2023-10-12 11:36:56 -0300

    Update metodo_split.py

--- Python/Semana05/metodo_split.py
+++ Python/Semana05/metodo_split.py
@@ -1,6 +1,9 @@
+# Parte 1: Santiago 
 # help(str.split)
 
 cursos = 'Java JavaScript Node Python Diseno'
 lista_cursos = cursos.split()
 print(f'Lista de cursos: {lista_cursos}')
 print(type(lista_cursos))
+
+# Parte 2 
