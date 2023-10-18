b57d996212dcd47e1e965177051a0984aa399e3b
Nati88 <113054513+Nati88@users.noreply.github.com>
2023-10-18 20:47:44 -0300

    Update dar_formato_str.py

--- Python/Semana06/dar_formato_str.py
+++ Python/Semana06/dar_formato_str.py
@@ -9,3 +9,11 @@ persona = ('Carla', 'Gómez', 5000.00)
 mensaje_con_formato = 'Hola %s %s .Tu sueldo es %.2f' # % persona # Aquí le pasamos el objeto que es tupla
 print(mensaje_con_formato%persona)
 
+nombre = 'Juan'
+edad = 19
+sueldo = 3000
+# mensaje_con_formato = 'Nombre {} Edad {} Sueldo {:.2f}'.format(nombre, edad, sueldo)
+# print(mensaje_con_formato)
+
+mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+print(mensaje)
